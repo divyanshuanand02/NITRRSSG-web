e1ffb9f12325db235200ca43732431373d35883c
unknown <swetadash0610@gmail.com>
2020-09-03 19:51:09 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+//using es6version
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  // console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+
+});
+
+var content = 'NIT ROURKELA Robosoccer Student Research Group';
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('#demo').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
+
+
